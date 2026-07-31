9e28d95bdcbabcf48e04f1ede03978db0cd1fcb0
heathdj <heathdj@gmail.com>
2026-07-31 15:22:53 -0500

    Iniital Commit of Xcode Generated Files

---
+++ Trip Planner/Trip Planner/Trip_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trip_PlannerApp.swift
+//  Trip Planner
+//
+//  Created by David Heath on 7/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Trip_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Trip Planner/Trip PlannerTests/Trip_PlannerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Trip_PlannerTests.swift
+//  Trip PlannerTests
+//
+//  Created by David Heath on 7/31/26.
+//
+
+import Testing
+@testable import Trip_Planner
+
+struct Trip_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
